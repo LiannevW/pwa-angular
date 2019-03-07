ef2a2765aee0614f2e1ef331d0aca75a1b206780
Lianne Van Westrhenen <lianne.vanwestrhenen@incentro.com>
2019-03-07 17:21:52 +0100

    add firebase request

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 import { SwUpdate } from '@angular/service-worker';
+import {AngularFirestore, AngularFirestoreCollection} from 'angularfire2/firestore';
+import {Observable} from 'rxjs';
+
+interface City {
+  id?: string;
+  name: string;
+  province: string;
+  population: number;
+  highlights?: string;
+  rating?: number;
+}
 
 @Component({
   selector: 'app-root',
@@ -9,12 +20,22 @@ import { SwUpdate } from '@angular/service-worker';
 export class AppComponent implements OnInit {
   title = 'pwa-angular-app';
 
-  constructor(private swUpdate: SwUpdate) {
+  cities$: Observable<City[]>;
+  citiesCollection: AngularFirestoreCollection<City>;
+
+  constructor(
+    private swUpdate: SwUpdate,
+    private  afs: AngularFirestore) {
   }
 
   ngOnInit() {
 
-    if (this.swUpdate.isEnabled) {
+    this.loadNewVersion();
+    this.getCities();
+    }
+
+    loadNewVersion() {
+      if (this.swUpdate.isEnabled) {
 
         this.swUpdate.available.subscribe(() => {
 
@@ -23,6 +44,11 @@ export class AppComponent implements OnInit {
                 window.location.reload();
             }
         });
+      }
     }
-}
+
+    getCities() {
+      this.citiesCollection = this.afs.collection('cities');
+      this.cities$ = this.citiesCollection.valueChanges();
+    }
 }
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,16 +1,13 @@
-// This file can be replaced during build by using the `fileReplacements` array.
-// `ng build --prod` replaces `environment.ts` with `environment.prod.ts`.
-// The list of file replacements can be found in `angular.json`.
-
 export const environment = {
-  production: false
+  production: false,
+  // Initialize Firebase
+  firebase: {
+    apiKey: 'API_KEY_FIREBASE',
+    authDomain: 'cities-app-firestore.firebaseapp.com',
+    databaseURL: 'https://cities-app-firestore.firebaseio.com',
+    projectId: 'cities-app-firestore',
+    storageBucket: 'cities-app-firestore.appspot.com',
+    messagingSenderId: '621166094347'
+  },
+  cities_endpoint: 'cities'
 };
-
-/*
- * For easier debugging in development mode, you can import the following file
- * to ignore zone related error stack frames such as `zone.run`, `zoneDelegate.invokeTask`.
- *
- * This import should be commented out in production mode because it will have a negative impact
- * on performance if an error is thrown.
- */
-// import 'zone.js/dist/zone-error';  // Included with Angular CLI.
