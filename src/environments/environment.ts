fe39a6256ef1b6c7d58fbbfec08797db545261d5
Lianne Van Westrhenen <lianne.vanwestrhenen@incentro.com>
2019-03-21 16:50:17 +0100

    refactor

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,7 +2,7 @@ export const environment = {
   production: false,
   // Initialize Firebase
   firebase: {
-    apiKey: 'AIzaSyB3-5TtPp4RqSv29Om4mycUx1ghw6PJVfc',
+    apiKey: 'API_KEY',
     authDomain: 'cities-app-firestore.firebaseapp.com',
     databaseURL: 'https://cities-app-firestore.firebaseio.com',
     projectId: 'cities-app-firestore',
